f1de6c3bf460a13148bd4a468470c2059a5724ad
waynebanks <thestorerun@yahoo.com>
2018-12-05 12:03:37 -0800

    added procfile

---
+++ index.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let app = express();
+const path = require("path");
+let html = require('html');
+let ejs = require('ejs');
+
+// app.set( 'view engine', 'html' );
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('/views', __dirname + '/views/')
+
+// app.use( bodyParser.json() );
+// app.use( bodyParser.urlencoded( {
+//   extended: false
+// } ) );
+
+
+app.get('/', (req, res) => {
+    console.log('checking in from home!')
+    res.render('../collection.ejs');
+})
+// res.sendFile(__dirname + '/index.html');
+
+app.get('/collection', (req, res) => {
+    console.log('checking in from collection!')
+    res.render('index.ejs');
+    // res.sendFile('/fileName.html');
+})
+let port = process.env.PORT || 3001;
+
+app.listen(port, () =>{
+  console.log('phase server is live on port:', port)
+})
+module.exports = app;
